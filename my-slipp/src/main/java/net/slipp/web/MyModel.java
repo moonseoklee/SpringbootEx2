f729d5017457f5a660c72719428993b1cd9d86f8
moonseoklee <winnyiee@korea.ac.kr>
2019-08-09 15:51:37 +0900

    add : add html

---
+++ my-slipp/src/main/java/net/slipp/web/MyModel.java
@@ -0,0 +1,16 @@
+package net.slipp.web;
+
+public class MyModel {
+	private String name;
+
+	public MyModel(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	
+	
+}
--- my-slipp/src/main/java/net/slipp/web/WelcomeController.java
+++ my-slipp/src/main/java/net/slipp/web/WelcomeController.java
@@ -1,5 +1,8 @@
 package net.slipp.web;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WelcomeController {
 	@GetMapping("/helloworld")
-	public String welcome(String name,int age,Model model) {
-		System.out.println("name : "+name+age);
-		model.addAttribute("name",name);
-		model.addAttribute("age",age);
+	public String welcome(Model model) {
+		List<MyModel> repo = Arrays.asList(new MyModel("model1"),new MyModel("model2"));
+		model.addAttribute("repo",repo);
+		
 		return "welcome";
 	}
 }
